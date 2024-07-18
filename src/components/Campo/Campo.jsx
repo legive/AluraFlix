150cc98319403c5ca99ab130c5ab3cf5652d2296
legive <leylagisela@gmail.com>
2024-07-17 21:57:48 -0600

    Responsive page cellphone

--- src/components/Campo/Campo.jsx
+++ src/components/Campo/Campo.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 import "./Campo.css"
 const Campo = (props) => {
-    const { titulo, placeholder, ancho, valor, actualizarValor } = props
+    const { titulo, placeholder, valor, actualizarValor } = props
      const manejarCambio = (e) => {
       actualizarValor(e.target.value);
      };
     return (
         <div className="campo">
             <label>{titulo}</label>
-            <input onChange={manejarCambio} value={valor} placeholder={placeholder} type="text" style={{ width: ancho }}
+            <input className="textbox" onChange={manejarCambio} value={valor} placeholder={placeholder} type="text" 
             ></input>
 
         </div>
